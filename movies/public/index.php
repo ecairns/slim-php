6ff9d6f01f621d25342158597a0a3d1bd35f0a37
Ethan Cairns <ecairns@oklahoman.com>
2019-02-07 17:15:37 -0700

    Abstract out routes from index.php into it's own file

--- movies/public/index.php
+++ movies/public/index.php
@@ -1,7 +1,4 @@
 <?php
-
-use \CCB\Controllers\API\FilmController;
-
 // Make file paths relative to project root
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
@@ -10,12 +7,9 @@ $settings = require 'app/config.php';
 $app      = new \Slim\App($settings);
 
 require 'app/src/dependencies.php';
+require 'app/src/routes.php';
 
-$container = $app->getContainer();
-
-$app->get('/movies',  "MovieApiController:search");
-
-// Register the database connection with Eloquent
+// Register Eloquent
 $capsule = $app->getContainer()->get('capsule');
 $capsule->bootEloquent();
 
